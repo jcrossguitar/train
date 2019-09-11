78f9b59402792d917bd2790ae808ca05ac342d26
jon <jcrossguitar@gmail.com>
2019-09-11 15:43:39 -0700

    I used a reference to get my firebase working without errors. There was a problem with my references. it is close to working, but not yet sending data to firebase.

--- train.js
+++ train.js
@@ -1,20 +1,28 @@
 
+//     var b = firebase.database().ref("messages");
+//         $("#submitContact").submit(function(a) { $(this), console.log("Submit to Firebase");
+//         var c = $("#name").val(),
+//             d = $("#destination").val(),
+//             x = $("#timeField").val(),
+//             y = $("#frequencyField").val(),
+//             f = { name: c, destination: d, timeField: x, frequencyField: y};
+//         return b.push(f).then(function(a) { 
+//             $(".sucess").css("display", "block"), 
+//             $(".sucess-none").css("display", "none") }), !1 })   
+// });
+
+// $(document).ready(function() {
+//     if ($('#newTrain').length > 0 ) {
+//         contactScript('forcontact');
+//     }
+// });
+
 
 $(document).ready(function() {
-    $("#submitContact").click(function(){
-                    console.log('search...')
-                            // creating a variable for search term
-        var nameField = $("#name").val();
-        var destinationField = $("#destination").val();
-        var timeField = $("#firstTime").val();
-        var frequencyField = $("#Frequency").val();
-        console.log(nameField);
-    });
-});
+  console.log("train file has loaded")
 
-
-function contactScript(value) {
-    var a = { 
+console.log(firebase);
+var firebaseConfig = { 
       apiKey: "AIzaSyCbkpARF1NLMvfj_wywe9qbfZfEJ8vTMYA",
 	    authDomain: "train-time-1f9c9.firebaseapp.com",
         databaseURL: "https://train-time-1f9c9.firebaseio.com",
@@ -23,21 +31,30 @@ function contactScript(value) {
         messagingSenderId: "170875927561",
         appId: "1:170875927561:web:13fcd1f6ad2078b2020cd8"
     };
+
     firebase.initializeApp(firebaseConfig);
 
-    firebase.initializeApp(a);
-    var b = firebase.database().ref("messages");
-        $("#newContact").submit(function(a) { $(this), console.log("Submit to Firebase");
-        var c = $("#name").val(),
-            d = $("#email").val(),
-            f = { name: c, email: d};
-        return b.push(f).then(function(a) { 
-            $(".sucess").css("display", "block"), 
-            $(".sucess-none").css("display", "none") }), !1 })   
-}
+    $("#submitContact").click(function(){
+        console.log('search...')
+                // creating a variable for search term
+var nameField = $("#name").val().trim();
+var destinationField = $("#destination").val().trim();
+var timeField = $("#firstTime").val().trim();
+var frequencyField = $("#Frequency").val().trim();
+var db = firebase.database();
 
-$(document).ready(function() {
-    if ($('#newTrain').length > 0 ) {
-        contactScript('forcontact');
-    }
+var createRow = function(data) {
+// Create a new table row element
+var tRow = $("<tr>");
+// Methods run on jQuery selectors return the selector they we run on
+// This is why we can create and save a reference to a td in the same statement we update its text
+var titleTd = $("<td>").text(data);
+// Append the newly created table data to the table row
+tRow.append(titleTd);
+// Append the table row to the table body
+$("tbody").append(tRow);
+};
+console.log(nameField);
+    });
+
 });
